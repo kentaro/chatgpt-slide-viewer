272213d2f1aecc8ba6e01163b4a5925beb1c8ad2
Kentaro Kuribayashi <kentarok@gmail.com>
2022-12-04 21:16:19 +0900

    create a pdf viewer component copy-and-pasted from ChatGPT

---
+++ components/MyPdfViwer.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Document, Page } from 'react-pdf';
+
+interface Props {
+  file: string; // PDFファイルのURL
+}
+
+const MyPdfViewer: React.FC<Props> = ({ file }) => {
+  const [numPages, setNumPages] = React.useState(0);
+  const [pageNumber, setPageNumber] = React.useState(1);
+
+  // PDFファイルのページ数を取得する
+  const onDocumentLoadSuccess = ({ numPages }) => {
+    setNumPages(numPages);
+  };
+
+  // ページ番号を変更する
+  const changePage = offset => {
+    setPageNumber(prevPageNumber => prevPageNumber + offset);
+  };
+
+  return (
+    <div>
+      <Document
+        file={file}
+        onLoadSuccess={onDocumentLoadSuccess}
+      >
+        <Page pageNumber={pageNumber} />
+      </Document>
+      <div>
+        Page {pageNumber} of {numPages}
+      </div>
+      <button onClick={() => changePage(-1)}>Previous</button>
+      <button onClick={() => changePage(1)}>Next</button>
+    </div>
+  );
+};
+
+export default MyPdfViewer;
